09ee8e290bf2d568c19f1e221f0811f6d299295d
Stevie Peck <peckstevie204@gmail.com>
2024-08-03 01:18:23 -0400

    feat: Implement human choice logic

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getComputerChoice() {
   // Call the random function to get a random number between 0 and 2
   // Select one of the 3 options based on the random number
-  
+
   switch (random(3)) {
     case 0:
       return "rock";
@@ -19,4 +19,33 @@ function getComputerChoice() {
 function random(num) {
   num *= Math.random();
   return Math.floor(num);
+}
+
+// getHumanChoice will ask the user to input "rock", "paper", or "scissors" and returns their answer
+function getHumanChoice() {
+  // Loop until a valid move is made
+  while(true) {
+    // Prompt the user to enter their move (case-insensitive)
+    let move = prompt("Rock, Paper, Scissors!\n");
+    move = move.toLowerCase();
+
+    // Include shorthand moves (r, p, s) for easier typing
+    // If the move is not valid, alert the user
+    switch (move) {
+      case "rock":
+      case "r":
+        return "rock";
+
+      case "paper":
+      case "p":
+        return "paper";
+
+      case "scissors":
+      case "s":
+        return "scissors";
+
+      default:
+        alert("That's not a valid move!");
+    }
+  }
 }
